12d7fa3e23ef9e48c9fbffa09725794d2520c4b2
rubymaxioned <axioned0482022>
2022-12-01 19:34:24 +0530

    added load functionlity

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,11 @@ var loadBtn = document.querySelector(".button");
 var counter = 0;
 var n = 6;
 var len = boxList.length;
-console.log(len);
-// console.log(loadBtn);
-// console.log(boxList);
+// console.log(len);
+
 
 function loadMoreFunction(initial, last) {
     for(var i=initial; i<last; i++){
-        counter++;
         boxList[i].classList.add('active');
     }
 }
@@ -19,15 +17,32 @@ loadMoreFunction(counter, n);
 
 
 loadBtn.addEventListener('click',function(){
-    // containerList.forEach(function(list){
-    //     containerList[counter].classList.add('active');
-    //     counter++;
-    //     console.log(list);
-    // })
-    if(counter < len) {
-        // counter = counter + 6;
+    // console.log(counter);
+    if(counter < len-6) {
+        counter = counter + 6;
         n += 6;
         loadMoreFunction(counter, n);
-       
+        
     } else {  loadBtn.classList.add('hide'); }
 })
+
+
+// console.log(loadBtn);
+// console.log(boxList);
+
+// function loadMoreFunction(initial, last) {
+//     for(var i=n; i>counter; i--){
+//         boxList[i].classList.add('active');
+//     }
+// }
+
+// loadMoreFunction(counter, n);
+
+// loadBtn.addEventListener('click',function(){
+//     if(counter < len) {
+//         counter = counter + 6;
+//         n += 6;
+//         loadMoreFunction(counter, n);
+       
+//     } else {  loadBtn.classList.add('hide'); }
+// })
