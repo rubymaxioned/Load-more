5bea8659d747e4c021874a643b21c6b05aa26550
rubymaxioned <axioned0482022>
2022-12-02 11:44:29 +0530

    json

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,35 @@
-var box = document.querySelector(".box");
-var boxList = document.querySelectorAll(".box li");
-var loadBtn = document.querySelector(".button");
-var counter = 0;
-var n = 6;
-var len = boxList.length;
-console.log(len);
+// var box = document.querySelector(".box");
+// var boxList = document.querySelectorAll(".box li");
+// var loadBtn = document.querySelector(".button");
+// var counter = 0;
+// var n = 6;
+// var len = boxList.length;
+// console.log(len);
+
+// function loadMoreFunction(initial, last) {
+// n = n < len ? n : len;   
+// for (var i = counter; i < n; i++) {
+//     if (i === len - 2) {
+//         loadBtn.classList.add('hide');
+//     }
+//     boxList[i].classList.add('active');
+
+// }
+// }
+// loadMoreFunction(counter, n);
+
+// loadBtn.addEventListener('click',function(){
+//                 counter = counter + 6;
+//                 n += 6;
+//                 loadMoreFunction(counter, n);
+// })
 
 
-function loadMoreFunction(initial, last) {
-n = n < len ? n : len;   
-for (var i = counter; i < n; i++) {
-    if (i === len - 2) {
-        loadBtn.classList.add('hide');
+let http = new XMLHttpRequest();
+http.open('get','https://jsonplaceholder.typicode.com/posts',true);
+http.send();
+http.onload = function(){
+    if(this.readyState == 4 && this.status == 200) {
+        let loremContainer = 
     }
-    boxList[i].classList.add('active');
-
 }
-}
-loadMoreFunction(counter, n);
-
-loadBtn.addEventListener('click',function(){
-                counter = counter + 6;
-                n += 6;
-                loadMoreFunction(counter, n);
-})
